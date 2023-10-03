ce37a4025c16ad91d1e55f086ab15804d7c14369
Divya R <kwrdivyaraj100@gmail.com>
2023-10-03 12:18:07 +0530

    get/post

--- routes/students-routes.js
+++ routes/students-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const  {homePage, insertValues} = require('../controllers/students-controllers.js');
+const { homePage, insertValues } = require('../controllers/students-controllers.js');
 
 const studentRouter = express.Router();
 

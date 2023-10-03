bcc24d37a4451adc757befe01edf1010b5ffc23a
Divya R <kwrdivyaraj100@gmail.com>
2023-10-03 12:15:58 +0530

    worked with connection

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ const studentRouter = require('./routes/students-routes.js');
 const connection = require('./model/connection.js');
 
 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use('/', studentRouter);
+
+
+connection.connect(err => {
+    if (err) console.log("There is an error while connecting" + err);
+
+    app.listen(4000);
+
+    console.log("DB Connected" + connection.threadId);
+    console.log("Server started at port 4000");
+
+})
 
 
 
